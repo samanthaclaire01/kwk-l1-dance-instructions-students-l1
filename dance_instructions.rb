8ec9388a61e93b5d83e00b2d1854c799cee9da25
Samantha Cassity <cassityclaire@gmail.com>
2018-06-05 16:04:12 +0000

    Done.

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,49 @@
-
-# Code your methods below
+def lower_Body
+#Assume the proper stance
+puts "Plant legs far apart, bend knees slightly and keep posture loose"
+#Base Footwork
+puts "Lift right foot"
+puts "Return right foot to the ground"
+puts "Finishing with a small skip-step backward"
+end
+lower_Body
+def skip_step
+  puts "Lower left foot to the ground"
+  puts "Bounce left foot back up slightly, kicking it a few inches back"
+  puts "Right, Left, Right and Right"
+end
+skip_step
+def upper_body
+  #bob the reigns
+  puts "Holding your arms out in front of you, straight and at chest level"
+  puts "Cross your right wrist over your left and hold them together"
+  dance = "Lift your arms uo and down in a loose bouncing movement, in time with the beat of the song"
+puts dance
+puts dance
+puts dance
+puts dance
+puts dance
+puts dance
+puts dance
+puts dance
+end
+upper_body
+def lasso 
+  #Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left
+  puts "Lift right arm up and point right elbow diagonally to the right"
+  lassie = "Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso"
+  puts lassie
+  puts lassie
+  puts lassie
+  puts lassie
+  puts lassie
+  puts lassie
+  puts lassie
+  puts lassie
+end
+lasso
+def bust_a_move
+  puts "be cool"
+  puts "be snazzy!"
+end
+bust_a_move
